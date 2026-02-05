1bb7185d0fd514db30a353be6b24d0275262b6ce
MariaChernokoz <me@chernokoz.ru>
2026-02-05 11:40:44 +0300

    feat: initial ios project structure with common gitignore

---
+++ ios/FinCoach/FinCoach/FinCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinCoachApp.swift
+//  FinCoach
+//
+//  Created by Chernokoz on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
